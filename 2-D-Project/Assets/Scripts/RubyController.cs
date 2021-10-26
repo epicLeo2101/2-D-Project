5341ab66b59ca24479abab104fc75df6c6ddc22c
epicLeo2101 <91509500+epicLeo2101@users.noreply.github.com>
2021-10-26 15:33:08 -0500

    Cinemachine and HUD

--- 2-D-Project/Assets/Scripts/RubyController.cs
+++ 2-D-Project/Assets/Scripts/RubyController.cs
@@ -5,32 +5,32 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
 
-    public float speed = 3.0f;
-
+      public float speed = 3.0f;
+    
     public int maxHealth = 5;
-
+    
     public GameObject projectilePrefab;
-
-    public int health { get { return currentHealth; } }
+    
+    public int health { get { return currentHealth; }}
     int currentHealth;
-
+    
     public float timeInvincible = 2.0f;
     bool isInvincible;
     float invincibleTimer;
-
+    
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-
+    
     Animator animator;
-    Vector2 lookDirection = new Vector2(1, 0);
-
+    Vector2 lookDirection = new Vector2(1,0);
+    
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-
+        
         currentHealth = maxHealth;
     }
 
@@ -39,32 +39,32 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
-
+        
         Vector2 move = new Vector2(horizontal, vertical);
-
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
             lookDirection.Set(move.x, move.y);
             lookDirection.Normalize();
         }
-
+        
         animator.SetFloat("Look X", lookDirection.x);
         animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
-
+        
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
-
-        if (Input.GetKeyDown(KeyCode.C))
+        
+        if(Input.GetKeyDown(KeyCode.C))
         {
             Launch();
         }
     }
-
+    
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
@@ -80,15 +80,16 @@ public class RubyController : MonoBehaviour
         {
             if (isInvincible)
                 return;
-
+            
             isInvincible = true;
             invincibleTimer = timeInvincible;
         }
-
+        
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
-
+    
     void Launch()
     {
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
